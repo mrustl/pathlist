1f8a8922627c324525d6aa784e1bf679780b8d2b
hsonne <hauke.sonnenberg@gmx.de>
2019-07-13 18:26:28 +0200

    Fix test

--- tests/testthat/test_function_pathlist.R
+++ tests/testthat/test_function_pathlist.R
@@ -3,10 +3,8 @@ test_that("Initialising a pathlist object works", {
   paths <- c("r/abc", "r/abc/d", "r/abc/e")
   pl <- pathlist::pathlist(paths = paths)
 
-  testthat::expect_identical(pl@root, "r/abc")
-  testthat::expect_identical(pl@folders, matrix(c("", "d", "e"), ncol = 1))
-  testthat::expect_identical(pl@depths, c(0L, 1L, 1L))
-
-  pathlist::pathlist(c("a", "a/b"))@depths
+  testthat::expect_identical(pl@root, "r")
+  testthat::expect_identical(pl@folders[, 1], c("abc", "abc", "abc"))
+  testthat::expect_identical(pl@folders[, 2], c("", "d", "e"))
+  testthat::expect_identical(pl@depths, c(1L, 2L, 2L))
 })
-
